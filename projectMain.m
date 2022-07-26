91d5da53ee4bf4f60c88dc4cd875fa63f23f10c2
Arrowstar <hardena@alumni.msoe.edu>
2022-07-26 14:33:43 -0500

    Added GA output function.

--- projectMain.m
+++ projectMain.m
@@ -37,12 +37,13 @@ simDriver = NNS_SimulationDriver(arena,false);
 
 %% Run GA to train agent
 fun = @(x) gaObjFunc(x, simDriver, ship);
+outputFunc = @(options,state,flag) gaOutputFunc(options,state,flag, ship);
 
 controllers = ship.components.getControllerComps();
 agent = controllers(1).getAgent();
 x = getXVectFromActor(agent);
 
-options = optimoptions("ga", "PopulationSize",12, "UseParallel",true, "Display","iter", "PlotFcn",{'gaplotscorediversity', 'gaplotbestf', 'gaplotdistance'}, 'MaxGenerations',2000, "FunctionTolerance",0);
+options = optimoptions("ga", "PopulationSize",8, "UseParallel",true, "OutputFcn",outputFunc, "Display","iter", "PlotFcn",{'gaplotscorediversity', 'gaplotbestf', 'gaplotdistance'}, 'MaxGenerations',2000, "FunctionTolerance",0);
 [x,fval,exitflag,output,population,scores] = ga(fun,numel(x),[],[],[],[],[],[],[],options);
 
 setXVectFromActor(agent, x);
@@ -60,7 +61,7 @@ function f = gaObjFunc(x, simDriver, nnShip)
         nnShip NNS_Ship
     end
 
-    numRuns = 2;
+    numRuns = 1;
     f = NaN(1,numRuns);
     for(i=1:numRuns) %#ok<*NO4LP> 
         %get the RL agent we're training and set its learnable values
@@ -76,4 +77,31 @@ function f = gaObjFunc(x, simDriver, nnShip)
     end
 
     f = mean(f);
+end
+
+function [state,options,optchanged] = gaOutputFunc(options,state,flag, nnShip)
+    if(strcmpi(flag,'iter'))
+        genNum = state.Generation;
+
+        scores = state.Score;
+        [bestScore,I] = min(scores);
+        x = state.Population(I,:);
+
+        controllers = nnShip.components.getControllerComps();
+        agent = controllers(1).getAgent();
+        setXVectFromActor(agent, x);
+
+        date = datestr(now(), 'YYYYmmDD_HHMMSS');
+        filename = sprintf('nnship_Gen_%u_Score_%0.0f_%s.mat', genNum, bestScore, date);
+        filepath = fullfile(pwd,'ships','nn_train',filename);
+
+        [folder,~,~] = fileparts(filepath);
+        if(not(exist(folder,"dir")))
+            mkdir(folder);
+        end
+
+        save(filepath, 'nnShip');
+    end
+
+    optchanged = false;
 end
