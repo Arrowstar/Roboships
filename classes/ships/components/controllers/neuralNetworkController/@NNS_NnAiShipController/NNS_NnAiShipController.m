78ee5869fe48e23765a23ff83b2402831748f07a
Arrowstar <hardena@alumni.msoe.edu>
2022-08-05 09:45:23 -0500

    Added NN ship w/256 nodes per layer

--- classes/ships/components/controllers/neuralNetworkController/@NNS_NnAiShipController/NNS_NnAiShipController.m
+++ classes/ships/components/controllers/neuralNetworkController/@NNS_NnAiShipController/NNS_NnAiShipController.m
@@ -29,7 +29,7 @@ classdef NNS_NnAiShipController < NNS_AbstractShipController & NNS_AbstractPower
             obsInfo = obj.ship.getObservationInfo();
             actInfo = obj.getActInfo();
 
-            initOpts = rlAgentInitializationOptions("NumHiddenUnit",8, "UseRNN",false);
+            initOpts = rlAgentInitializationOptions("NumHiddenUnit",256, "UseRNN",false);
             obj.rlAgent = rlPPOAgent(obsInfo,actInfo,initOpts);
             obj.rlAgent.UseExplorationPolicy = false;
         end
